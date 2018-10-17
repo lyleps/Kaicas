d426e6a0298ddb8c21d076eb646bd13cd2178d59
LylePS <LylePS@gmail.com>
2018-10-17 19:57:38 -0400

    Have an Eval class. Also made all number tokend not have underscores when stored

---
+++ src/Eval.java
@@ -0,0 +1,76 @@
+/**
+   The Eval class,
+   containing static methods for evaluating Cell structures
+**/
+public class Eval
+{
+	/**
+	   Evaluates the given cell structure
+	   The Cell structure should not be modified
+	   @param root The root Cell of the cell structure
+	   @return An evaluated Cell structure
+	**/
+	public static Cell evaluate(Cell root)
+	{
+		if(root == null)
+			return null;
+		Cell newRoot = root.clone();
+		return eval(newRoot);
+	}
+
+	/*
+	  A helper method for evaluate that recursively evaluates an Cell structure
+	  @param root The root of the Cell structure
+	  @return The evaluated Cell structure
+	*/
+	private static Cell eval(Cell root)
+	{
+		if (root == null) return null;
+		if (root.getDown() == null) return root;
+		String func = root.getDown().getData();
+		if (func == null) return root;
+		if(func.equals("head")) return head(eval(head(tail(root))));
+		if(func.equals("tail")) return tail(eval(head(tail(root))));
+		if(func.equals("concat"))
+			return concat(eval(head(tail(root))), eval(head(tail(tail(root)))));
+		return root;
+	}
+
+	/*
+	  Get the head of the given Cell structure
+	  @param root The root of the Cell structure
+	  @return The head of the given Cell structure
+	*/
+	private static Cell head(Cell root)
+	{
+		if (root == null) return null;
+		return root.getDown();
+	}
+
+	/*
+	  Get the tail of the given Cell structure
+	  @param root The root of the Cell structure
+	  @return The tail of the given Cell structure
+	*/
+	private static Cell tail(Cell root)
+	{
+		if (root == null) return null;
+		return root.getOver();
+	}
+
+	/*
+	  Concatenate the rootB Cell structure to the end of the rootA Cell structure
+	  @param rootA The root of the first Cell structure
+	  @param rootB The root of the second Cell structure
+	  @return The rootB Cell structure to the end of the root A Cell structure
+	*/
+	private static Cell concat(Cell rootA, Cell rootB)
+	{
+		if (rootA == null) return rootB;
+		Cell tempA = rootA;
+		while(tail(tempA) != null) tempA = tail(tempA);
+		tempA.setOver(rootB);
+		return rootA;
+	}
+	
+}
